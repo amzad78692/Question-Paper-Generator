a0b4f17270c8c285e4d4441e66dec7bbf17fccc2
nirajp3 <nirajpandey7634848806@gmail.com>
2022-02-25 14:47:57 +0530

    Some Change

--- faculty.php
+++ faculty.php
@@ -5,7 +5,7 @@
            <meta http-equiv="X-UA-Compatible" content="IE=edge">
            <meta name="viewport" content="width=device-width, initial-scale=1.0">
            <link rel="stylesheet" href="facultylogin.css">
-           <title>Faculty Login Page</title>
+           <title>Faculty Login</title>
 </head>
 <body>
          <div class="container">                             
