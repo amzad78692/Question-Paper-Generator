49cb50ee63c390f1c53b767765f623bb7c2b696d
nirajp3 <nirajpandey7634848806@gmail.com>
2022-02-25 14:23:33 +0530

    Faculty Menu and Admin Menu created

---
+++ Admin.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+                  <meta charset="UTF-8">
+                  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                  <link rel="stylesheet" href="Admin.css">
+                  <title>Department Login Page</title>
+</head>
+<body> 
+    <div class="container">
+        <h1 id="heading">Question Paper Generator</h1>
+        <p id="paragraph">(Welcome to Admin)</p>
+        <div id="logo">
+            <img src="Admin-Logo.png" alt="Admin">
+        </div>
+        <div class="main">
+            <a href="level1">Id for exam dept</a>
+            <a href="level2">Generate ques paper</a>
+            <a href="level3">View ques paper</a>
+            <a href="level4">Change own password</a>
+            <a href="level5">View id password</a>
+            <a href="level6">Log out</a>
+        </div>
+
+    </div>
+    <div class="box">
+    </div>
+                  
+</body>
+</html>
---
+++ faculty.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+           <meta charset="UTF-8">
+           <meta http-equiv="X-UA-Compatible" content="IE=edge">
+           <meta name="viewport" content="width=device-width, initial-scale=1.0">
+           <link rel="stylesheet" href="facultylogin.css">
+           <title>Faculty Login Page</title>
+</head>
+<body>
+         <div class="container">                             
+             <h1 id="heading"> Question Paper Generator</h1>
+             <p id="paragraph">(Welcome to Faculty)</p>
+             <div class="facultymain">
+                 <a href="">Create Question</a>
+                 <a href="">Edit Question</a>
+                 <a href="">View Question</a>
+                 <a href="">Delete Question</a>
+                 <a href="">Log Out</a>
+             </div>
+        </div>
+        <div class="box">
+            
+        </div>
+                  
+</body>
+</html>
