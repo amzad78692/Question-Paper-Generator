028224e31cab1aca8ba2ffdd99e2a802dc20349e
MD Amzad <md.amzad.site.btech.cse.2@subhartiuniversit.onmicrosoft.com>
2022-02-25 12:55:41 +0530

    First Changes

---
+++ AdminLogin.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Login</title>
+    <link rel="stylesheet" href="AdminLogin.css">
+</head>
+<body>
+    <section>
+        <div class="color"></div>
+        <div class="color"></div>
+        <div class="color"></div>
+        <div class="box">
+            <div class="square" style="--i:0;"></div>
+            <div class="square" style="--i:1;"></div>
+            <div class="square" style="--i:2;"></div>
+            <div class="square" style="--i:3;"></div>
+            <div class="square" style="--i:4;"></div>
+            <div class="container">
+                <div class="form">
+                    <form>
+                        <h2>Admin Login</h2>
+                        <form>
+                            <div class="inputbox">
+                                <input type="text" placeholder="Username">
+                            </div>
+                            <div class="inputbox">
+                                <input type="password" placeholder="Password">
+                            </div>
+                            <div class="inputbox">
+                                <input type="submit" value="Login">
+                            </div>
+                        </form>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MainPage.css">
+    <link href="https://fonts.googleapis.com/css2?family=Teko:wght@300&display=swap" rel="stylesheet">
+    <title>Question Paper Generator</title>
+</head>
+<body>
+    <div class="Heading">
+        <div id="Logo">
+            <img src="./Images/logo.png" alt="Subharti">
+        </div>
+        <h1>Subharti Institute of Technology &
+             Engineering</h1>
+        <p id="Est">Established...2008 </p>
+        <div id="Naac-logo">
+            <img src="./Images/logo2.png" alt="NAAC-A">
+
+        </div>
+        <div id="MainMenu">
+            <a href="AdminLogin.php" id="Alogin">Admin Login</a>
+            <a href="#" id="Edlogin">Examination Department Login</a>
+            <a href="#" id="Flogin">Faculty Login</a>
+            <a href="#" id="About">About</a>
+        </div>
+    </div>
+    <div class="Content">
+        <img src="./Images/img1.png" alt="Image">
+    </div>
+</body>
+</html>
