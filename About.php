33a3aa7974d47481e88033c4ce98ba4791e1ac51
nirajp3 <nirajpandey7634848806@gmail.com>
2022-02-26 23:29:48 +0530

    niraj3

---
+++ About.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+       <meta http-equiv="X-UA-Compatible" content="IE=edge">
+       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+       <link rel="stylesheet" href="Aboutt.css">
+       <title>About</title>
+</head>
+<body>
+      <div class="container">
+         <h1 id="heading">About</h1>
+         
+            <div class="box">
+            <div id="logo">
+       <img src="./Image/bmwlogo.png" alt="image">
+     </div>
+              <div class="box2">
+              <p Id="paragraph">Lorem ipsum dolor sit amet consectetur adipisicing elit            
+              voluptate nisi, quis error doloremque
+              ipsam expedita voluptatum illo sapiente facilis molestiae amet!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores
+              adipisci molestias consequuntur, voluptas doloribus
+              doloremque dolor, atque excepturi dolore aperiam  
+              a ea dicta consectetur ipsam ratione, cumque nostrum quos itaque?
+            </p>
+
+              </div>
+            </div>
+            
+     </div>
+     
+                                              
+</body>
+</html>
