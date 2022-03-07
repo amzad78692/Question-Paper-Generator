84299ff9ac7897f483adff7bb8d962a01799803e
MD Amzad <md.amzad.site.btech.cse.2@subhartiuniversit.onmicrosoft.com>
2022-03-07 16:56:31 +0530

    path change

--- createpassword.php
+++ createpassword.php
@@ -11,7 +11,7 @@
 
 <body>
     <div class="container">
-        <img src="passimg.jpg" alt="Background Image">
+        <img src="Images/passimg.jpg" alt="Background Image">
         <div class="newpasswordform">
             <h2 id="heading">Update Password</h2>
             <form method="post" id="form">
