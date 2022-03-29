9dc3b0c7ea2f613dd70447873a2071f84ef61379
nirajp3 <nirajpandey7634848806@gmail.com>
2022-03-29 12:01:04 +0530

    some

--- Examdptcngpass.php
+++ Examdptcngpass.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="Examdeptchngpass.css">
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script>import swal from 'sweetalert';</script>
         <title>Exam Dept Change Password</title>
 </head>
 <body>
@@ -31,3 +33,41 @@
                   
 </body>
 </html>
+
+<?php
+    if(isset($_POST['submit']))
+    {
+        $oldpass = $_POST['oldpass'];
+        $newpass = $_POST['newpass'];
+        $connect = mysqli_connect("localhost", "root", "");
+        $connect->select_db("questionpaper");
+        $query = " select password from examdept where password = '".$oldpass."'";
+        $result = $connect->query($query);
+
+        if($result->num_rows > 0)
+        {
+            $query = "update examdept set password = '".$newpass."' where password = '".$oldpass."'"; 
+            $connect->query($query);
+            ?>
+            <script>
+                swal("Password update successfull !", "", "success")
+                .then((value) => {
+                    window.location.replace("Examdpt.php");
+                });
+            </script>
+            <?php
+        }
+        else
+        {
+            ?>
+            <script>
+                swal("Invalid old password !", "", "error")
+                .then((value) => {
+                    window.location.replace("Examdpt.php");
+                });
+            </script>
+            <?php
+        }
+        $connect->close();
+    }
+?>
--- Examdptviewpass.php
+++ Examdptviewpass.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="Examdptviewpas.css">
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script>import swal from 'sweetalert';</script>
         <title>Exam Dept View Id Password</title>
 </head>
 <body>
@@ -12,13 +14,49 @@
         <div class="dptviewpass">
             <h1 id="heading">View Id Password</h1>
             <form method="post" id="form">
-                <p id="first"> Enter Faculty Name</p>
-                <input type="text" placeholder="Faculty Name" name="Name" required>
-                <p id="second"> Enter Department Name</p>
-                <input type="text" placeholder="Department Name" name="Name" required>
+                <p class="first"> Enter Subject Code</p>
+                <input type="text" placeholder="Subject Code" name="code" required>
                 <div id="button">
-                <input type="submit" value="Submit"  name="view">
+                <input type="submit" value="View"  name="submit">
+                <?php
+                    if(isset($_POST['submit']))
+                        getIdPass();
+                ?>
                 </div>
             </form>        
         </div>
     </div>
+</body>
+
+
+<?php
+    function getIdPass()
+    {
+        $code = $_POST['code'];
+        $connect = mysqli_connect("localhost", "root", "");
+        $connect->select_db("questionpaper");
+
+        $query = "select fname, username, password from registerfaculty where scode = '".$code."'";
+        $result = $connect->query($query);
+
+        if($result->num_rows >0)
+        {
+            $data = $result->fetch_assoc();
+            echo "<br>";
+            echo "<p class='first'> Faculty Name</p> = ".$data['fname'];
+            echo "<br>";
+            echo "<p class='first'> Username</p> = ".$data['username'];
+            echo "<br>";
+            echo "<p class='first'> Password</p> = ".$data['password'];
+        }
+        else
+        {
+            ?>
+            <script>
+                swal("Invalid Subject Code !", "", "error");
+            </script>
+            <?php
+        }
+        $connect->close();
+    }
+?>
