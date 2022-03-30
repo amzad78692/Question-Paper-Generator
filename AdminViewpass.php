3f39de7b6acc4f2f6e22e48ac101599db30f0b36
nirajp3 <nirajpandey7634848806@gmail.com>
2022-03-30 19:17:26 +0530

    some change

--- AdminViewpass.php
+++ AdminViewpass.php
@@ -5,6 +5,8 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="AdminViewidpass.css">
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script>import swal from 'sweetalert';</script>
         <title>Admin View Id Password</title>
 </head>
 <body>
@@ -13,10 +15,14 @@
         <div class="viewpass">
             <h1 id="heading">View Id Password</h1>
             <form method="post" id="form">
-                <p id="para"> Enter College Code</p>
+                <p class="para"> Enter College Code</p>
                 <input type="text" placeholder="College Code" name="ccode" required>
                 <div id="button">
-                <input type="submit" value="Submit"  name="view">
+                <input type="submit" value="View"  name="submit">
+                 <?php
+                    if(isset($_POST['submit']))
+                      adminviewpass();
+                 ?>
                 </div>
             </form>        
         </div>
@@ -25,3 +31,36 @@
                   
 </body>
 </html>
+
+<?php
+   function adminviewpass()
+   {
+    $code = $_POST['ccode'];
+    $connect = mysqli_connect("localhost", "root", "");
+    $connect->select_db("questionpaper");
+
+    $query = "select ccode, username, password from examdept where ccode = '".$code."'";
+    $result = $connect->query($query);
+
+    if($result->num_rows >0)
+    {
+        $data = $result->fetch_assoc();
+        echo "<br>";
+        echo "<p id='para'>College Code</p> =".$data['ccode'];
+        echo "<br>";
+        echo "<p id='para'> Username</p> =".$data['username'];
+        echo "<br>";
+        echo "<p id='para'> Password</p>".$data['password'];
+    }
+    else
+    {
+        ?>
+        <script>
+            swal("Invalid College Code !", "", "error");
+        </script>
+        <?php
+    }
+    $connect->close();
+
+   }
+?>
