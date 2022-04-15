845fec130079af85eef0301514a587a83b6e5807
nirajp3 <nirajpandey7634848806@gmail.com>
2022-04-15 21:08:40 +0530

    changes

--- Insertquestion.php
+++ Insertquestion.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     <script>import swal from 'sweetalert';</script>
+    <link rel="stylesheet" href="Insertquestion.css">
     <title>Insert Question</title>
 </head>
 <body>
